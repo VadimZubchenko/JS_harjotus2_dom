8817a84dc5bf707c96e62fedfba0655a5fcf624e
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2022-04-24 22:11:11 +0300

    added install instraction of typescript into readMe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JS_harjotus2_dom
+
+- How to make a transfer Javascript to Typescript https://youtu.be/qFMMOJucqTw :
+- npx tsc --init (to create tsconfig.json, then uncomment: ”outDoor”: ”./dist”, server.js is placed”rootDir”: ”./”, ”moduleResolution”…)
+- npm -D typescript @types/node @types/express @types/nodemon (to adding into devDependcies)
+- change the server.js to server.ts
+- add to package.json: - ”scripts": { - "start": "nodemon dist/server.js", - "build": "tsc -p tsconfig.json" - },
+- npm run build (it compile the server.ts to /dist/server.js, this command has been set in package.json)
+- npm start
--- public/script.js
+++ public/script.js
@@ -4,7 +4,9 @@ function changeColor() {
   let letters = "ABCDEF0123456789";
   for (let i = 0; i < 6; i++) {
     let temp = Math.floor(Math.random() * 16); // floor delete all after ,
+    //each iteration color grows up by one of letters[array index]
     color = color + letters[temp];
+    console.log(color);
   }
   header.style.color = color;
   console.log(color);
@@ -12,16 +14,22 @@ function changeColor() {
 
 function toggleBox() {
   let domButton = document.getElementById("dombutton");
+  // get div node from DOM-html
   let anchor = document.getElementById("anchor");
   if (domButton.value === "Show") {
     domButton.value = "Hide";
+    // create a new div-element with size and color
     let box = document.createElement("div");
     box.style.height = "200px";
     box.style.width = "200px";
     box.style.backgroundColor = "red";
+    //put the id into the "div"
+    box.id = "redbox";
+    // Append the box node to the "div" node:
     anchor.appendChild(box);
   } else {
     domButton.value = "Show";
+    // get element from DOM, and if it exists, then remove it.
     let redbox = document.getElementById("redbox");
     if (redbox) {
       anchor.removeChild(redbox);
